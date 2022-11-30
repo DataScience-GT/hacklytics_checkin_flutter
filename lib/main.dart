2b4fd45937a02a76958c2bb83154ae942c647f53
JohnRamberger <jramberger3@gatech.edu>
2022-11-29 22:38:51 -0500

    infinite list

--- lib/main.dart
+++ lib/main.dart
@@ -37,9 +37,22 @@ class RandomWords extends StatefulWidget {
 }
 
 class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18);
+
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: /*1*/ (context, i) {
+        if (i.isOdd) return const Divider(); /*2*/
+
+        final index = i ~/ 2; /*3*/
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10)); /*4*/
+        }
+        return Text(_suggestions[index].asPascalCase);
+      },
+    );
   }
 }
