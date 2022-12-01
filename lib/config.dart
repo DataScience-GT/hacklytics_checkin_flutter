b790ab6b5962e2694d30ff2588d4b8f8a937754f
JohnRamberger <jramberger3@gatech.edu>
2022-11-30 20:57:57 -0500

    Update config.dart

--- lib/config.dart
+++ lib/config.dart
@@ -1,6 +1,10 @@
 class Config {
-  static const asd = "";
+  String a = "";
+
+  ///The singleton instance of the config
   static final Config _instance = Config._internal();
+
+  ///The singleton instance of the config
   factory Config() {
     return _instance;
   }
