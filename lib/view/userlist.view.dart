a483ada65d82a1616469f9cd63c9bc4cf0c81bf2
JohnRamberger <jramberger3@gatech.edu>
2022-12-03 17:09:41 -0500

    Create userlist.view.dart

---
+++ lib/view/userlist.view.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class UserListView extends StatefulWidget {
+  const UserListView({super.key});
+
+  @override
+  State<UserListView> createState() => _UserListViewState();
+}
+
+class _UserListViewState extends State<UserListView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
