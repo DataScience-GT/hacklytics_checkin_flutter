c61691c83d5a2f7f60a242f0666bdf2599f50a56
JohnRamberger <jramberger3@gatech.edu>
2022-12-03 17:05:44 -0500

    Update read.nfc.view.dart

--- lib/view/read.nfc.view.dart
+++ lib/view/read.nfc.view.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
 
-import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+// import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
 import 'package:amplify_flutter/amplify_flutter.dart';
 import "package:flutter/material.dart";
 import 'package:hacklytics_checkin_flutter/model/record.dart';
