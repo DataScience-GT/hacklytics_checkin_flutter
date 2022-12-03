789564e674645a2c3d84cc3b3b539b60816e8a01
JohnRamberger <jramberger3@gatech.edu>
2022-12-02 23:30:01 -0500

    Update read.nfc.view.dart

--- lib/view/read.nfc.view.dart
+++ lib/view/read.nfc.view.dart
@@ -1,3 +1,5 @@
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+import 'package:amplify_flutter/amplify_flutter.dart';
 import "package:flutter/material.dart";
 import 'package:hacklytics_checkin_flutter/model/record.dart';
 
@@ -12,25 +14,47 @@ class ReadNfcView extends StatefulWidget {
 
 class _ReadNfcViewState extends State<ReadNfcView> {
   bool _isReading = true;
+  bool _checkedRecords = false;
   late ReadNfc _nfc;
+  bool _isHacklytics = false;
+  late WellknownTextRecord _hacklyticsRecord;
+  bool _loadingUser = false;
 
   @override
   Widget build(BuildContext context) {
+    if (!_isReading && !_checkedRecords) {
+      // check if has hacklytics record
+      var match = _nfc.records
+          .where((r) =>
+              r is WellknownTextRecord && r.text.contains("hacklytics://"))
+          .toList();
+      if (match.isNotEmpty) {
+        _isHacklytics = true;
+        _hacklyticsRecord = match[0] as WellknownTextRecord;
+        _loadingUser = true;
+        // load the user from amplify
+      } else {
+        _isHacklytics = false;
+      }
+      // we have now checked records
+      _checkedRecords = true;
+    }
     return Scaffold(
         appBar: AppBar(
           title: const Text('NFC'),
         ),
-        body: Center(
-            child: _isReading
-                ? ScanDialog(onRead: (nfc) {
-                    print("nfc found");
-                    print(nfc);
-                    setState(() {
-                      _nfc = nfc;
-                      _isReading = false;
-                    });
-                  })
-                : const Text("nfc found.")));
+        body: _isReading
+            ? Center(child: ScanDialog(onRead: (nfc) {
+                setState(() {
+                  _nfc = nfc;
+                  _isReading = false;
+                });
+              }))
+            : (!_isHacklytics
+                ? const Center(child: Text("No Hacklytics Record."))
+                : (_loadingUser
+                    ? const Center(child: CircularProgressIndicator())
+                    : Text('record: ${_hacklyticsRecord.text}'))));
   }
 }
 
