5bc85e745d53ade7c3652d9c26de1d34a059e563
JohnRamberger <jramberger3@gatech.edu>
2022-12-03 17:45:11 -0500

    Update userlist.view.dart

--- lib/view/userlist.view.dart
+++ lib/view/userlist.view.dart
@@ -1,5 +1,10 @@
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'dart:convert';
+
+import 'package:amplify_flutter/amplify_flutter.dart';
+
+import 'package:flutter/material.dart';
+
+import 'package:hacklytics_checkin_flutter/model/user.dart';
 
 class UserListView extends StatefulWidget {
   const UserListView({super.key});
@@ -9,8 +14,80 @@ class UserListView extends StatefulWidget {
 }
 
 class _UserListViewState extends State<UserListView> {
+  bool _loading = true;
+  late List<dynamic> _users;
+  late String _error = "";
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    if (_loading == true) {
+      getUsers((data, error) {
+        if (error != null && error.isNotEmpty) {
+          setState(() {
+            _error = error;
+            _loading = false;
+          });
+        } else {
+          setState(() {
+            _users = data;
+            _loading = false;
+          });
+        }
+      });
+    }
+
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Users"),
+        ),
+        body: _loading
+            ? const Center(child: CircularProgressIndicator())
+            : ListView.builder(
+                itemCount: _users.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(_users[index].attributes["name"]),
+                    subtitle: Text(_users[index].attributes["email"]),
+                  );
+                },
+              ));
+  }
+
+  getUsers(Function(dynamic data, dynamic error) callback) async {
+    // hacklytics record is of the form hacklytics://<id>
+    var request = GraphQLRequest(document: '''
+          query {
+            listUsers
+          }
+          ''');
+    var response = await Amplify.API.query(request: request).response;
+    var data = response.data;
+    // var getUserById = data['getUserById'];
+    var res = ResponseListUsers(data);
+    callback(res.users, res.error);
+  }
+}
+
+class ResponseListUsers {
+  late List<dynamic> users = [];
+  late String error = "";
+  ResponseListUsers(String data) {
+    // print(data);
+    var json = jsonDecode(data);
+    var listUsers = jsonDecode(json['listUsers']);
+    var statusCode = listUsers['statusCode'];
+    var body = listUsers['body'];
+    if (statusCode == 200) {
+      // success
+      List<dynamic> usersStr = body['users'];
+
+      for (var userStr in usersStr) {
+        User user = User(userStr);
+        users.add(user);
+      }
+    } else {
+      // error
+      error = body["error"].toString();
+    }
   }
 }
