bc951010f5f3ed711480cb5e98694add1592dbe9
JohnRamberger <jramberger3@gatech.edu>
2022-12-04 03:27:33 -0500

    removed padding

--- lib/components/ListViewCard.component.dart
+++ lib/components/ListViewCard.component.dart
@@ -16,13 +16,11 @@ class ListViewCard extends StatelessWidget {
         position: BadgePosition.topEnd(top: 2, end: 2),
         borderRadius: BorderRadius.circular(10),
         child: Card(
-            child: Padding(
-          padding: const EdgeInsets.only(left: 8, right: 8),
           child: ListView(
             shrinkWrap: true,
             physics: const ClampingScrollPhysics(),
             children: children,
           ),
-        )));
+        ));
   }
 }
