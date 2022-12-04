34ee39cc3c946eb2a1cfbf5a0df2497c771a85a3
JohnRamberger <jramberger3@gatech.edu>
2022-12-03 21:13:42 -0500

    changed status error type to dynamic

--- lib/model/status.dart
+++ lib/model/status.dart
@@ -1,10 +1,15 @@
+import 'package:hacklytics_checkin_flutter/config.dart';
+
 class Status {
   late String message;
   late bool success;
-  late Error error;
+  late dynamic error;
 
   Status({required this.message, required this.success});
   Status.withError({required this.error}) {
+    if (Config.isDebug) {
+      print(error);
+    }
     success = false;
   }
   Status.withSuccess({required this.message}) {
