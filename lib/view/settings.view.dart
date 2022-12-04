bf8a8935aaae37cfc94d134a8ac145419bfd8d45
JohnRamberger <jramberger3@gatech.edu>
2022-12-04 02:00:16 -0500

    shows package info

--- lib/view/settings.view.dart
+++ lib/view/settings.view.dart
@@ -1,17 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:hacklytics_checkin_flutter/model/amplifyuser.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
-class SettingsView extends StatelessWidget {
-  const SettingsView({super.key});
+class SettingsView extends StatefulWidget {
+  const SettingsView({required this.user, super.key});
+
+  final AmplifyUser user;
+
+  @override
+  State<SettingsView> createState() => _SettingsViewState();
+}
+
+class _SettingsViewState extends State<SettingsView> {
+  bool _loadingVersion = true;
+  late PackageInfo _packageInfo;
 
   @override
   Widget build(BuildContext context) {
+    if (_loadingVersion) {
+      PackageInfo.fromPlatform().then((value) {
+        setState(() {
+          _packageInfo = value;
+          _loadingVersion = false;
+        });
+      });
+    }
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Settings"),
-      ),
-      body: const Center(
-        child: Text("Settings"),
-      ),
+        appBar: AppBar(
+          title: const Text("Settings"),
+        ),
+        body: _buildWithVersionInfo());
+  }
+
+  _buildWithVersionInfo() {
+    return _loadingVersion
+        ? const Center(child: CircularProgressIndicator())
+        : _buildBody();
+  }
+
+  _buildBody() {
+    return Column(
+      children: [
+        ListTile(
+          title: Text("AppName"),
+          subtitle: Text(_packageInfo.appName),
+        ),
+        ListTile(
+          title: Text("PackageName"),
+          subtitle: Text(_packageInfo.packageName),
+        ),
+        ListTile(
+          title: Text("Version"),
+          subtitle: Text(_packageInfo.version),
+        ),
+        ListTile(
+          title: Text("BuildNumber"),
+          subtitle: Text(_packageInfo.buildNumber),
+        ),
+      ],
     );
   }
 }
