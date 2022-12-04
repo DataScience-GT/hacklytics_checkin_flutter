4705c3ab8b15c1c653dd4bcfcc829d82d7a22010
JohnRamberger <jramberger3@gatech.edu>
2022-12-04 03:40:48 -0500

    change style

--- lib/components/ListViewCard.component.dart
+++ lib/components/ListViewCard.component.dart
@@ -1,5 +1,6 @@
 import 'package:badges/badges.dart';
 import 'package:flutter/material.dart';
+import 'package:hacklytics_checkin_flutter/components/HeadingListTile.component.dart';
 
 class ListViewCard extends StatelessWidget {
   const ListViewCard({this.labelText = "", required this.children, super.key});
@@ -23,18 +24,30 @@ class ListViewCard extends StatelessWidget {
   }
 
   _buildWithLabel() {
-    return Badge(
-        badgeContent: Text(labelText),
-        shape: BadgeShape.square,
-        alignment: Alignment.topRight,
-        position: BadgePosition.topEnd(top: 2, end: 2),
-        borderRadius: BorderRadius.circular(10),
-        child: Card(
-          child: ListView(
-            shrinkWrap: true,
-            physics: const ClampingScrollPhysics(),
-            children: children,
-          ),
-        ));
+    return Card(
+      child: ListView(
+        shrinkWrap: true,
+        physics: const ClampingScrollPhysics(),
+        children: [
+          HeadingListTile(labelText: labelText),
+          // const Divider(),
+          ...children
+        ],
+      ),
+    );
   }
 }
+
+// Badge(
+// badgeContent: Text(labelText),
+// shape: BadgeShape.square,
+// alignment: Alignment.topRight,
+// position: BadgePosition.topEnd(top: 2, end: 2),
+// borderRadius: BorderRadius.circular(10),
+// child: Card(
+//   child: ListView(
+//     shrinkWrap: true,
+//     physics: const ClampingScrollPhysics(),
+//     children: children,
+//   ),
+// ));
