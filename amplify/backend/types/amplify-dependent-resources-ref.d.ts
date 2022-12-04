0594609bc02c568dbd64d7b4ff5f224f04af11ad
JohnRamberger <jramberger3@gatech.edu>
2022-12-03 22:24:53 -0500

    backend

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,22 +1,4 @@
 export type AmplifyDependentResourcesAttributes = {
-    "auth": {
-        "hacklyticsportal2023": {
-            "IdentityPoolId": "string",
-            "IdentityPoolName": "string",
-            "UserPoolId": "string",
-            "UserPoolArn": "string",
-            "UserPoolName": "string",
-            "AppClientIDWeb": "string",
-            "AppClientID": "string"
-        }
-    },
-    "api": {
-        "hacklyticsportal2023": {
-            "GraphQLAPIKeyOutput": "string",
-            "GraphQLAPIIdOutput": "string",
-            "GraphQLAPIEndpointOutput": "string"
-        }
-    },
     "storage": {
         "imagestorage": {
             "BucketName": "string",
