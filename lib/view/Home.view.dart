cbd9ad7fbb926d4bad2205192901090d3cba63a4
JohnRamberger <jramberger3@gatech.edu>
2022-12-03 21:13:50 -0500

    added user info getting

--- lib/view/Home.view.dart
+++ lib/view/Home.view.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
 import 'package:amplify_authenticator/amplify_authenticator.dart';
 import 'package:amplify_flutter/amplify_flutter.dart';
@@ -7,6 +9,8 @@ import 'package:hacklytics_checkin_flutter/components/test.nfc.dart';
 import 'package:hacklytics_checkin_flutter/view/nfc.view.dart';
 
 import '../config.dart';
+import '../model/AmplifyUser.dart';
+import '../model/status.dart';
 
 /// The home page
 class HomeView extends StatefulWidget {
@@ -18,12 +22,15 @@ class HomeView extends StatefulWidget {
 
 // TODO - use FutureBuilder to show loading indicator while getting the user's groups
 class _HomeViewState extends State<HomeView> {
+  late AmplifyUser _user;
+
   @override
   Widget build(BuildContext context) {
-//     final session =
-//     await Amplify.Auth.fetchAuthSession() as CognitoAuthSession;
-// final idToken = session.userPoolTokens!.idToken;
-// final userGroups = idToken.groups;
+    getUserInfo((status) {
+      if (status.success) {
+        print('message: ${status.message}');
+      }
+    });
 
     return Scaffold(
       appBar: AppBar(
@@ -37,10 +44,35 @@ class _HomeViewState extends State<HomeView> {
             ElevatedButton(
                 onPressed: () => Navigator.push(context,
                     MaterialPageRoute(builder: (context) => NfcView())),
-                child: Text("Nfc"))
+                child: const Text("Nfc"))
           ],
         ),
       ),
     );
+  }
+
+  getUserInfo(Function(Status status) callback) async {
+    try {
+      AuthSession authSessions = await Amplify.Auth.fetchAuthSession(
+          options: CognitoSessionOptions(getAWSCredentials: true));
+
+      if (authSessions.isSignedIn) {
+        final accessToken =
+            (authSessions as CognitoAuthSession).userPoolTokens?.accessToken;
+        if (accessToken == null || accessToken.isEmpty) {
+          callback(Status.withError(error: "No access token."));
+          return;
+        }
+
+        var attributes = await Amplify.Auth.fetchUserAttributes();
+
+        _user = AmplifyUser(accessToken: accessToken, attributes: attributes);
+
+        callback(Status.withSuccess(message: "Got user info."));
+      }
+    } catch (err) {
+      return callback(Status.withError(error: err));
+    }
+    // return callback(Status.withSuccess(message: "Success!"));
   }
 }
