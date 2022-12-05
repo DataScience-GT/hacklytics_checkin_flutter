00dc350533b6b7974784b4dd61460f0e17d4d004
JohnRamberger <jramberger3@gatech.edu>
2022-12-04 20:15:21 -0500

    Update CheckinUserList.view.dart

--- lib/view/CheckinUserList.view.dart
+++ lib/view/CheckinUserList.view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:hacklytics_checkin_flutter/components/statuscard.component.dart';
+import 'package:hacklytics_checkin_flutter/model/status.dart';
 import 'package:hacklytics_checkin_flutter/models/Event.dart';
 
 class CheckinUserListView extends StatefulWidget {
@@ -11,15 +13,43 @@ class CheckinUserListView extends StatefulWidget {
 }
 
 class _CheckinUserListState extends State<CheckinUserListView> {
+  bool _loadingCheckins = true;
+  List<dynamic> _checkins = [];
+  String _error = "";
+
   @override
   Widget build(BuildContext context) {
+    if (_loadingCheckins) {
+      // get list of checkins
+      _getCheckins((status) => {
+            setState(() => {_loadingCheckins = false, _error = status.error})
+          });
+    }
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Checkin User List"),
-      ),
-      body: const Center(
-        child: Text("Checkin User List"),
-      ),
+        appBar: AppBar(
+          title: const Text("Checkin User List"),
+        ),
+        body: _loadingCheckins
+            ? const Center(child: CircularProgressIndicator())
+            : (_error.isNotEmpty
+                ? StatusCard(message: _error, success: false)
+                : _buildWithCheckins()));
+  }
+
+  _buildWithCheckins() {
+    return ListView.builder(
+      itemCount: _checkins.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Text(_checkins[index]["user"]["name"]),
+          subtitle: Text(_checkins[index]["user"]["email"]),
+        );
+      },
     );
+  }
+
+  _getCheckins(Function(Status) callback) async {
+    callback(Status.withError(error: "Not implemented"));
   }
 }
