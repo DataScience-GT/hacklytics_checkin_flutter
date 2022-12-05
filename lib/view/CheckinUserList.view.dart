6170a876b3debf325fa6e49778dfecdc98778dc3
JohnRamberger <jramberger3@gatech.edu>
2022-12-04 21:06:14 -0500

    Update CheckinUserList.view.dart

--- lib/view/CheckinUserList.view.dart
+++ lib/view/CheckinUserList.view.dart
@@ -1,7 +1,11 @@
+import 'package:amplify_api/amplify_api.dart';
+import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:hacklytics_checkin_flutter/components/statuscard.component.dart';
 import 'package:hacklytics_checkin_flutter/model/status.dart';
 import 'package:hacklytics_checkin_flutter/models/Event.dart';
+
+import 'package:hacklytics_checkin_flutter/models/Checkin.dart';
 
 class CheckinUserListView extends StatefulWidget {
   const CheckinUserListView({required this.event, super.key});
@@ -21,9 +25,13 @@ class _CheckinUserListState extends State<CheckinUserListView> {
   Widget build(BuildContext context) {
     if (_loadingCheckins) {
       // get list of checkins
-      _getCheckins((status) => {
-            setState(() => {_loadingCheckins = false, _error = status.error})
-          });
+      // _getCheckins((status) => {
+      //       setState(() => {_loadingCheckins = false, _error = status.error})
+      //     });
+      var x = _getCheckinsStream();
+      x.listen((event) {
+        print(event);
+      });
     }
 
     return Scaffold(
@@ -50,6 +58,33 @@ class _CheckinUserListState extends State<CheckinUserListView> {
   }
 
   _getCheckins(Function(Status) callback) async {
-    callback(Status.withError(error: "Not implemented"));
+    try {
+      final queryPredicate = Checkin.EVENT.eq(widget.event.id);
+      final request =
+          ModelQueries.list(Checkin.classType, where: queryPredicate);
+      final response = await Amplify.API.query(request: request).response;
+      if (response.errors.isNotEmpty) {
+        return callback(Status.withError(error: response.errors.toString()));
+      }
+      var data = response.data;
+      print(data);
+
+      callback(Status.withSuccess(message: "Not implemented"));
+    } catch (e) {
+      callback(Status.withError(error: e.toString()));
+    }
+  }
+
+  Stream<GraphQLResponse<Checkin>> _getCheckinsStream() {
+    final subscriprionRequest = ModelSubscriptions.onCreate(Checkin.classType);
+    final Stream<GraphQLResponse<Checkin>> operation = Amplify.API
+        .subscribe(subscriprionRequest,
+            onEstablished: () => {print("subscription established")})
+        .handleError((e) => {
+              setState(() => {
+                    _error = e.toString(),
+                  })
+            });
+    return operation;
   }
 }
