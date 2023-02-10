1ecfb5c76ece33ad3a3151eeabd00f38a61a651d
JohnRamberger <jramberger3@gatech.edu>
2023-02-10 06:25:45 -0500

    getuserbyname

--- amplify/backend/function/hacklyticsportalGetUserByName/src/index.js
+++ amplify/backend/function/hacklyticsportalGetUserByName/src/index.js
@@ -10,14 +10,75 @@ const AWS = require("aws-sdk");
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
 exports.handler = async (event) => {
-    console.log(`EVENT: ${JSON.stringify(event)}`);
-    return {
-        statusCode: 200,
-    //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*",
-    //      "Access-Control-Allow-Headers": "*"
-    //  }, 
-        body: JSON.stringify('Hello from Lambda!'),
+  try {
+    var params = {
+      UserPoolId: process.env.AUTH_HACKLYTICSPORTAL2023_USERPOOLID,
+      AttributesToGet: ["name"],
     };
+
+    AWS.config.update({
+      region: process.env.USER_POOL_REGION,
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_KEY,
+    });
+    var cognitoidentityserviceprovider =
+      new AWS.CognitoIdentityServiceProvider();
+    let x = await new Promise((resolve, reject) => {
+      cognitoidentityserviceprovider.listUsers(params, (err, data) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(data);
+        }
+      });
+    });
+    var users = x.Users;
+    if (users.length > 0) {
+      var user =
+        users.filter((x) =>
+          x?.attributes?.name?.toLowerCase().includes(event.arguments.userName)
+        ) ?? false;
+      if (user) {
+        return JSON.stringify({
+          statusCode: 200,
+          //  Uncomment below to enable CORS requests
+          //  headers: {
+          //      "Access-Control-Allow-Origin": "*",
+          //      "Access-Control-Allow-Headers": "*"
+          //  },
+          body: { ok: 1, users: user },
+        });
+      } else {
+        return JSON.stringify({
+          statusCode: 400,
+          //  Uncomment below to enable CORS requests
+          //  headers: {
+          //      "Access-Control-Allow-Origin": "*",
+          //      "Access-Control-Allow-Headers": "*"
+          //  },
+          body: { ok: 0, error: "User not found." },
+        });
+      }
+    } else {
+      return JSON.stringify({
+        statusCode: 500,
+        //  Uncomment below to enable CORS requests
+        //  headers: {
+        //      "Access-Control-Allow-Origin": "*",
+        //      "Access-Control-Allow-Headers": "*"
+        //  },
+        body: { ok: 0, error: "No users found in user pool." },
+      });
+    }
+  } catch (err) {
+    return JSON.stringify({
+      statusCode: 500,
+      //  Uncomment below to enable CORS requests
+      //  headers: {
+      //      "Access-Control-Allow-Origin": "*",
+      //      "Access-Control-Allow-Headers": "*"
+      //  },
+      body: { ok: 0, error: err.message },
+    });
+  }
 };
